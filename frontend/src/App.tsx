34a480a824cd3a5727c1ecb0553ccbf2dbc91e26
vannhat <st.bread3rd@gmail.com>
2025-12-03 00:29:51 +0700

    Xay dung Admin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,11 +6,12 @@ import * as StaffPages from "./pages/Staff";
 import * as AuthPages from "./pages/Auth";
 
 import LayoutKhachHang from "./components/layout/LayoutKhachHang";
+import LayoutAdmin from "./components/layout/LayoutAdmin";
 
 function App() {
   return (
     <Routes>
-      {/* ----------------------------------------------------------------------- */}
+      {/* ----------------------------CLIENT---------------------------- */}
       <Route element={<LayoutKhachHang />}>
         {/* 2. ROUTE CLIENT */}
         <Route path="/trang-chu" element={<ClientPages.TrangChu />} />
@@ -19,10 +20,19 @@ function App() {
         <Route path="/lien-he" element={<ClientPages.LienHe/>} />
         <Route path="/gio-hang" element={<ClientPages.GioHang />} />
       </Route>
+      {/* ----------------------------ADMIN---------------------------- */}
+      <Route element={<LayoutAdmin />}>
+        {/* 2. ROUTE Admin */}
+        <Route path="/quan-ly-tai-khoan" element={<AdminPages.QuanLyNguoiDung />} />
+        <Route path="/quan-ly-san-pham" element={<AdminPages.QuanLySanPham/>} />
+        <Route path="/quan-ly-don-hang" element={<AdminPages.QuanLyDonHang/>} />
+      </Route>
+      {/* ----------------------------STAFF---------------------------- */}
+      <Route path="ho-tro" element={<StaffPages.HoTroKhachHang />} />
       {/* ------------------------------------------------------------------------ */}
       <Route path="/dang-nhap" element={<AuthPages.DangNhap />} />
       <Route path="/dang-ky" element={<AuthPages.DangKy />} />
-
+      
     </Routes>
   );
 }
